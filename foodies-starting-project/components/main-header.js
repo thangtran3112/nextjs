55530d9fc4e2dd1d3d6556ce3b3ea301f7fb66cd
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-04-21 19:26:38 -0700

    Next Image component for auto lazy loading

--- foodies-starting-project/components/main-header.js
+++ foodies-starting-project/components/main-header.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <Image src={logoImg} alt="A plate with food on it" priority />
         Next Level Food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
