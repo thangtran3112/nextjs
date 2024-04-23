12a5689db3403a697be09e435e4232a6403e0c64
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-04-22 21:51:35 -0700

    skip rendering with NextJS notFound()

--- foodies-starting-project/lib/meals.js
+++ foodies-starting-project/lib/meals.js
@@ -4,6 +4,11 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   //artificial delay to test loading
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return db.prepare("SELECT * FROM meals").all();
+}
+
+// it is not mandatory to wrap the db action in an async function
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
