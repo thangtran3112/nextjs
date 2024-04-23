7ebf541ddf1441cd2ade48f020cfb3e2da88068c
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-04-22 21:28:26 -0700

    Add not-found.js and error.js with Suspense

--- foodies-starting-project/app/meals/loading-backup.js
+++ foodies-starting-project/app/meals/loading-backup.js
@@ -1,4 +1,6 @@
 import classes from "./loading.module.css";
+
+// loading will automatically be injected into the page when page and its children are loaded
 export default function MealsLoadingPage() {
   return <p className={classes.loading}>Fetching Meals...</p>;
 }
