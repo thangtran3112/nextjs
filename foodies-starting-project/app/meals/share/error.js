7556bdbded8f089c2e1cec200291334c73747da8
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-04-24 22:05:50 -0700

    Submit form component

---
+++ foodies-starting-project/app/meals/share/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occured</h1>
+      <p>Fail to create a meal</p>
+    </main>
+  );
+}
