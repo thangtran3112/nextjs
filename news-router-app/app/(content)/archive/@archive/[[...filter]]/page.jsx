8f1b6aa5bc1f0c2f31c66de6b8052e514550f882
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-05-04 20:10:54 -0700

    Granular Suspense for partial re-rendering

--- news-router-app/app/(content)/archive/@archive/[[...filter]]/page.jsx
+++ news-router-app/app/(content)/archive/@archive/[[...filter]]/page.jsx
@@ -6,34 +6,11 @@ import {
   getNewsForYearAndMonth,
 } from "@/lib/news";
 import Link from "next/link";
+import { Suspense } from "react";
 
-export default async function FilteredNewsPage({ params }) {
-  //catch all dynamic routes and include archive/, e.g. /archive/2024, /archive/2023/02
-  const filter = params.filter;
-
-  const selectedYear = filter?.[0];
-  const selectedMonth = filter?.[1];
-
-  let news;
-  let links = await getAvailableNewsYears();
-
-  if (selectedYear && !selectedMonth) {
-    news = await getNewsForYear(selectedYear);
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (selectedYear && selectedMonth) {
-    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
-    links = []; // reset to not display year/months links to avoid /archive/2024/02/2024, etc
-  }
-
-  let newsContent = <p>No news available for the selected period.</p>;
-
-  if (news && news.length > 0) {
-    newsContent = <NewsList news={news} />;
-  }
-
+async function FilterHeader({ selectedYear, selectedMonth }) {
   const availableYears = await getAvailableNewsYears();
+  let links = availableYears;
 
   if (
     (selectedYear && !availableYears.includes(selectedYear)) ||
@@ -43,26 +20,74 @@ export default async function FilteredNewsPage({ params }) {
     throw new Error("Invalid filter");
   }
 
+  if (selectedYear && !selectedMonth) {
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    links = []; // reset to not display year/months links to avoid /archive/2024/02/2024, etc
+  }
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => {
+            const href = selectedYear
+              ? `/archive/${selectedYear}/${link}`
+              : `/archive/${link}`;
+
+            return (
+              <li key={link}>
+                <Link href={href}>{link}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+async function FilteredNews({ selectedYear, selectedMonth }) {
+  let news;
+  if (selectedYear && !selectedMonth) {
+    news = await getNewsForYear(selectedYear);
+  } else if (selectedYear && selectedMonth) {
+    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
+  }
+
+  let newsContent = <p>No news available for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return newsContent;
+}
+
+export default async function FilteredNewsPage({ params }) {
+  //catch all dynamic routes and include archive/, e.g. /archive/2024, /archive/2023/02
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
   return (
     <>
-      <header id="archive-header">
-        <nav>
-          <ul>
-            {links.map((link) => {
-              const href = selectedYear
-                ? `/archive/${selectedYear}/${link}`
-                : `/archive/${link}`;
-
-              return (
-                <li key={link}>
-                  <Link href={href}>{link}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
-      {newsContent}
+      <Suspense fallback={<p>Loading filter...</p>}>
+        <FilterHeader
+          selectedYear={selectedYear}
+          selectedMonth={selectedMonth}
+        />
+      </Suspense>
+      <Suspense fallback={<p>Loading news...</p>}>
+        {/* register this component in Suspense, if it is loading. The default
+        `loading.jsx` will not work on partial re-rendering without Suspense */}
+        <FilteredNews
+          selectedYear={selectedYear}
+          selectedMonth={selectedMonth}
+        />
+      </Suspense>
     </>
   );
 }
