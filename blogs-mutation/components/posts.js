43173fc15a8834ad3edfd05e7cb90bd0d9d0ba04
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-05-05 19:08:10 -0700

    use formAction to submit a button and revalidate path

--- blogs-mutation/components/posts.js
+++ blogs-mutation/components/posts.js
@@ -1,5 +1,6 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { togglePostLikeStatus } from "@/actions/posts";
 
 function Post({ post }) {
   return (
@@ -12,14 +13,20 @@ function Post({ post }) {
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form
+              // pass arg to function to be executed in the future
+              action={togglePostLikeStatus.bind(null, post.id)}
+              className={post.isLiked ? "liked" : ""}
+            >
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
