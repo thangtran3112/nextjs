71e02ba4f26b1b02ffd3e9f0bd3cec5601a33f3d
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-05-11 12:27:38 -0700

    Create a Protected Route with Lucia

--- next-auth/lib/auth.js
+++ next-auth/lib/auth.js
@@ -27,3 +27,50 @@ export async function createAuthSession(userId) {
     sessionCookie.attributes
   );
 }
+
+export async function verifyAuth() {
+  const sessionCookie = cookies().get(lucia.sessionCookieName);
+
+  if (!sessionCookie) {
+    //or return false;
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const sessionId = sessionCookie.value;
+
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  //look into db and validate the sessionId
+  const result = await lucia.validateSession(sessionId);
+
+  //refresh cookie, so user will not log out
+  //NextJS does not like we setting cookies while rendering a page, so we need try-catch
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch {}
+
+  return result;
+}
